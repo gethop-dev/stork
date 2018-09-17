2ac23a6d5df69a663e5e21b123902a8c0b95b64e
Damian Hryniewicz <damian@magnet.coop>
2018-09-17 12:10:23 +0200

    require lein 2.8.1

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "http://github.com/magnetcoop/stork"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.8.1"
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [com.datomic/datomic-free "0.9.5697"]
                                   [org.clojure/tools.namespace "0.2.3"]]
                    :source-paths ["dev"]}}
-  :deploy-repositories [["snapshots" {:url      "https://clojars.org/repo"
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password}]
-                        ["releases"  {:url      "https://clojars.org/repo"
+                        ["releases"  {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password}]])
