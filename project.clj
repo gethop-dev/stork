a5faa44b2dc5ebbc28be946da33b21fc03099796
spietras <szymon.pietras@magnet.coop>
2022-05-16 15:56:48 +0100

    Bump eastwood, cljfmt, clj and lein dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/gethop-dev/stork"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.8.1"
-  :plugins [[jonase/eastwood "0.3.4"]
-            [lein-cljfmt "0.6.2"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+  :min-lein-version "2.9.8"
+  :plugins [[jonase/eastwood "1.2.3"]
+            [lein-cljfmt "0.8.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]
                                   [com.datomic/datomic-free "0.9.5697"]]
                    :source-paths ["dev"]}}
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
